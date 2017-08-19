55ea2fb3a484300c4065c3aedc7b7aea26f8d7bc
bottasai <botta.saisrinivasa@gmail.com>
2017-08-19 23:48:36 +0530

    share analytical sql queries 2

--- dbscripts/ShareAnalyticalQeueries.sql
+++ dbscripts/ShareAnalyticalQeueries.sql
@@ -13,6 +13,8 @@ where date < curdate()-1
 and date > curdate()-12
 group by symbol
 
+select * from shares.volumeAvgs10days
+
 # Holding averages 10 days 
 create or replace view shares.Last10daysAvgs as 
 select b.symbol
@@ -28,10 +30,9 @@ where  v.symbol = b.symbol
 and b.date = curdate() -1
 # Shares with 10 days low higher than hostoricalAvgprice, close price higher than historical high
 #tradeqty greter than avg historical volume
-select * from shares.Last10daysAvgs l,shares.volumeAvgs v
-where l.last > v.high
-and l.low > v.avgprice
-and tradedqty > v.avgVolume*1.5
+select l.* from shares.Last10daysAvgs l,shares.volumeAvgs v
+where l.low > v.avgprice
+and l.avgvolume > v.avgVolume * 2
 and tradedqty > 100000
 and l.symbol = v.symbol
-order by tradedqty desc
+order by tradedqty desc	
