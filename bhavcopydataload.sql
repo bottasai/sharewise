8c3cd4180b44bd2f5463dec758e92deb84c9f720
root <botta.saisrinivasa@gmail.com>
2019-07-20 07:22:15 +0000

    merge

--- bhavcopydataload.sql
+++ bhavcopydataload.sql
@@ -1,8 +1,4 @@
-<<<<<<< HEAD
 LOAD DATA LOCAL INFILE './datafeeds/after/bhavcopy.csv' 
-=======
-LOAD DATA LOCAL INFILE './data/bhavcopy.csv' 
->>>>>>> df70eee93dd76070191fb569bd98733276f0c7ca
 INTO TABLE bhavdata 
 FIELDS TERMINATED BY ',' 
 ENCLOSED BY '"'
